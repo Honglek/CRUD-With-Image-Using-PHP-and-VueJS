29d42ed92d01f07c5ec6312bc40951be2bf24392
Honglek <160992141+Honglek@users.noreply.github.com>
2024-07-16 11:27:21 +0700

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <student />
+  </div>
+</template>
+
+<script>
+import student from "@/components/student.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    student,
+  },
+};
+</script>
